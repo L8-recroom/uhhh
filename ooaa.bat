a5066c0f89afe939c3bdcea14283b80b92259025
L..8 <lalalalucas40@gmail.com>
2026-01-31 13:22:42 -0500

    gvbhj

--- ooaa.bat
+++ ooaa.bat
@@ -1,17 +1,9 @@
 @echo off
-goto coloring
 echo !!WARNING!!
-set /p warning=This is a dangerous file that can cause permenant damage to your computer. I (the creator) AM NOT RESPONSIBLE FOR ANY DAMAGES! Are you sure you want to execute this file? either type "yes" or "no". THIS IS YOUR ONLY WARNING!:
+set /p warning=This is a dangerous file that can cause permenant damage to your computer. I (the creator) AM NOT RESPONSIBLE FOR ANY DAMAGES! Are you sure you want to execute this file? either type "yes" or "no". THIS IS YOUR ONLY WARNING!: 
 
 if /i "%warning%"=="yes" goto execution
 if /i "%warning%"=="no" goto nothing
-
-:coloring
-timeout /t 1 >nul
-color 0C
-timeout /t 1 >nul
-color 0F 
-goto coloring
 
 :execution
 echo i wasnt responsible i warned you lol > "C:\Windows\System32\config\OSDATA.txt"
